e5d8baf7abdf00735dd7e6fd7722e3025c826324
Taj Pereira <taj@canva.com>
2026-07-20 09:13:24 +1000

    fix: forward Coder auth to Octo

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -11,6 +11,14 @@ return {
     opts = {
       picker = "snacks",
       enable_builtin = true,
+      gh_env = function()
+        return {
+          CODER = vim.env.CODER,
+          CODER_AGENT_URL = vim.env.CODER_AGENT_URL,
+          CODER_AGENT_TOKEN = vim.env.CODER_AGENT_TOKEN,
+          CODER_AGENT_AUTH = vim.env.CODER_AGENT_AUTH,
+        }
+      end,
     },
   },
 }
